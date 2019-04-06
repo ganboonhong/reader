32d0f4a819a76cd24674489b675c92641846db98
Gan Boon Hong <ganboonhong@gmail.com>
2019-04-06 11:22:33 +0800

    add top headline filter

--- service/article_service.go
+++ service/article_service.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"html/template"
+	"log"
+	"io/ioutil"
 	"net/http"
 	// "reflect"
 	"strconv"
@@ -29,38 +30,47 @@ type ArticleData struct {
 
 type GetArticlesParam struct {
 	article_sources []string
+	country string
+	news_type string
 	s_date time.Time
 	e_date time.Time
 	page int
 }
 
-type Source struct {
-	source_name string `json:"name"`
-}
+const (
+	PageSize = 10
+)
 
 func (a ArticleService) GetArticles(param GetArticlesParam) (model.ArticleResult, error) {
 	c := newsapi.NewClient("07751a198b5440929cd22fc907b10389", newsapi.WithHTTPClient(http.DefaultClient))
+	var b []byte
+	var result *newsapi.ArticleResponse
+	var err error
 
-	// articles, err := c.GetTopHeadlines(context.Background(), &newsapi.TopHeadlineParameters{
-		// Sources: []string{"techcrunch", "cnn", "time"},
-	// })
+	switch param.news_type {
+	case "everything":
+		EverythingParameters := &newsapi.EverythingParameters{
+			Sources: param.article_sources,
+			From: param.s_date,
+			To: param.e_date,
+			Page: param.page,
+			PageSize: PageSize,
+			Language: "en",
+		}
+		result, err = c.GetEverything(context.Background(), EverythingParameters)
 
-	result, err := c.GetEverything(context.Background(), &newsapi.EverythingParameters{
-		// Sources: []string{"techcrunch", "cnn", "time"},
-		Sources: param.article_sources,
-		From: param.s_date,
-		To: param.e_date,
-		Page: param.page,
-		PageSize: 10,
-		Language: "en",
-		SortBy: "popularity",
-	})
-
-	if err != nil {
-		panic(err)
+	case "topheadline":
+		TopHeadlineParameters := &newsapi.TopHeadlineParameters{
+			// Sources: []string{"techcrunch", "cnn", "time"},
+			// Sources: param.article_sources,
+			Page: param.page,
+			PageSize: PageSize,
+			Country: param.country,
+		}
+		result, err = c.GetTopHeadlines(context.Background(), TopHeadlineParameters)
 	}
 
-	b, err := json.Marshal(result.Articles)
+	b, err = json.Marshal(&result.Articles)
 
 	var Articles []model.Article
 	err = json.Unmarshal(b, &Articles)
@@ -77,7 +87,7 @@ func (a ArticleService) GetArticles(param GetArticlesParam) (model.ArticleResult
 	return ArticleResult, nil
 }
 
-func (a ArticleService) GetStaticArticles()(*model.ArticleResult, error){
+func (a ArticleService) GetStaticArticles(param GetArticlesParam)(*model.ArticleResult, error){
 	b, err := ioutil.ReadFile("debug.json")
 	if err != nil {
 		return nil, fmt.Errorf("could not fetch static articles: %v", err)
@@ -106,27 +116,36 @@ func (a ArticleService) ArticlePageHandler(w http.ResponseWriter, r *http.Reques
 
 func (a ArticleService) GetArticleHandler(w http.ResponseWriter, r *http.Request){
 	ArticleService := ArticleService{}
+	log.SetFlags(log.Lshortfile)
 
 	query := r.URL.Query()
-	
-	s_date, err := time.Parse(time.RFC3339, query["e_date"][0] + "T00:00:00Z")
-	e_date, err := time.Parse(time.RFC3339, query["e_date"][0] + "T00:00:00Z")
-	
+	s_date, err := time.Parse(time.RFC3339, query["s_date"][0] + "T00:00:00+08:00")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	e_date, err := time.Parse(time.RFC3339, query["e_date"][0] + "T23:59:59+08:00")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
 	page, _ := strconv.Atoi(query["page"][0])
 	page += 1;
 	
 	param := GetArticlesParam{
-		s_date : s_date,
-		e_date : e_date,
-		page : page,
+		s_date: s_date,
+		e_date: e_date,
+		page: page,
+		news_type: query["news_type"][0],
 		article_sources : query["article_sources[]"],
+		country : query["country"][0],
 	}
-	result, err := ArticleService.GetArticles(param)
-	
-	// result, err := ArticleService.GetStaticArticles()
 
+	// result, err := ArticleService.GetStaticArticles(param)
+	result, err := ArticleService.GetArticles(param)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 
@@ -147,7 +166,7 @@ func (a ArticleService) GetArticleHandler(w http.ResponseWriter, r *http.Request
 	jsonStr, err := json.Marshal(data)
 
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
 
--- static/js/lib/Article.js
+++ static/js/lib/Article.js
@@ -3,14 +3,26 @@
 class Article {
     constructor() {
         this.daterangepickerId = "daterangepicker";
+        this.table;
+        // this.newsType = Article.CONSTANT.NEWS_TYPE.EVERYTHING;
+        this.newsType = Article.CONSTANT.NEWS_TYPE.TOPHEADLINE;
 
         this._initDateRangePicker(); // _initMainTable's dependency
         this._initArticleSourceSelect2();
+        this._initTopHeadlineSelect2();
         this._initMainTable();
         this._bindMenuButton();
         this._bindSubmitSourceFilter();
+        this._bindSubmitTopHeadlineFilter();
+    }
 
-        this.table;
+    static get CONSTANT() {
+        return {
+            NEWS_TYPE: {
+                EVERYTHING: "everything",
+                TOPHEADLINE: "topheadline",
+            }
+        }
     }
 
     _initMainTable() {
@@ -30,8 +42,10 @@ class Article {
                         s_date: $(`#${_t.daterangepickerId}`).data('daterangepicker').startDate.format('YYYY-MM-DD'),
                         e_date: $(`#${_t.daterangepickerId}`).data('daterangepicker').endDate.format('YYYY-MM-DD'),
                         article_sources: _t._getSourceFilter(),
+                        country: _t._getTopHeadlineFilter(),
                         page: $('#mainTable').DataTable().page(),
-                        dt: dt
+                        dt: dt,
+                        news_type: _t.newsType
                     };
                 }
             },
@@ -70,23 +84,32 @@ class Article {
     _initDateRangePicker() {
         const _t = this;
         const $daterangepicker = $(`#${this.daterangepickerId}`)
-        $daterangepicker.daterangepicker();
-
-        $daterangepicker.on("apply.daterangepicker", function(e, picker) {
-            _t.table.ajax.reload();
-        })
+        $daterangepicker.daterangepicker({
+            locale: {
+                format: "DD/MM/YY"
+            }
+        });
     }
 
     _initArticleSourceSelect2() {
         const _t = this;
         const $select2 = $('#sidebar-wrapper .article_source');
         $select2.select2({
-            placeholder: "Please select a source",
+            placeholder: "Select source",
             width: "95%",
             multiple: true,
         });
         $select2.val('').change();
         $('#sidebar-wrapper #comprehensive').val('cnn').change(); // default source
+    }
+
+    _initTopHeadlineSelect2() {
+        const _t = this;
+        const $select2 = $('#sidebar-wrapper .top_headline_select2');
+        $select2.select2({
+            placeholder: "Select country",
+            width: "95%",
+        });
     }
 
     _bindMenuButton() {
@@ -96,6 +119,18 @@ class Article {
         });
     }
 
+    _bindSubmitTopHeadlineFilter() {
+        const _t = this;
+        $("#sidebar-wrapper").on("click", "#submit_top_headline_filter", function() {
+            if (!_t._getTopHeadlineFilter()) {
+                toastr.warning("Please select country")
+                return;
+            }
+            _t.newsType = Article.CONSTANT.NEWS_TYPE.TOPHEADLINE;
+            _t.table.ajax.reload();
+        })
+    }
+
     _bindSubmitSourceFilter() {
         const _t = this;
         $("#sidebar-wrapper").on("click", "#submit_source", function() {
@@ -103,6 +138,7 @@ class Article {
                 toastr.warning("Please select a source")
                 return;
             }
+            _t.newsType = Article.CONSTANT.NEWS_TYPE.EVERYTHING
             _t.table.ajax.reload();
         })
     }
@@ -121,8 +157,12 @@ class Article {
         return sources;
     }
 
+    _getTopHeadlineFilter() {
+        return $("#sidebar-wrapper .top_headline_select2").val();
+    }
+
     _getTitleEle(data, row) {
-        let str = `[<b>${row.Source.name}</b>] `;
+        let str = `<b>${row.Source.name}</b> - `;
         str += data;
         return str;
     }
